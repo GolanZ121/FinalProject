a4044f2561ecf2d98475f71edf769527d1785c8d
GolanZ121 <zivg121@gmail.com>
2026-04-23 11:41:42 +0300

    simple script added

---
+++ tests.m
@@ -0,0 +1,19 @@
+%% Scripts to examine the samples
+clc; clear; close all;
+% Params
+Fs = 10e6;
+
+% load the raw_samples file 
+filename = 'raw_samples_12_Feb_2026_09_30_40_676_fs_10MHz.32fc';
+fid = fopen(filename, 'r');
+
+% Read all data as 32-bit floats
+data = fread(fid, inf, 'float32'); 
+fclose(fid);
+numSamples = length(data);
+TotalTime = numSamples / Fs;
+% Reshape data into a time series format
+
+time = (0:numSamples-1) / Fs;
+plot(time, [real(data), imag(data)]);
+legend("Real", "Imaginary");
